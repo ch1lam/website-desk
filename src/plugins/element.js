f8d5e48f78c2d093abb89e0e44a68231d2dfdd4c
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-03-24 00:29:23 +0800

    Update element.js

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,37 @@
 import Vue from "vue";
-import { Button } from "element-ui";
-import { Input } from "element-ui";
-import { Form } from "element-ui";
-import { Row } from "element-ui";
-import { Col } from "element-ui";
+
+import {
+  Button,
+  Input,
+  Form,
+  Row,
+  Col,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Link,
+  Icon,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Avatar
+} from "element-ui";
 
 Vue.use(Button);
 Vue.use(Input);
 Vue.use(Form);
 Vue.use(Row);
 Vue.use(Col);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
+Vue.use(Link);
+Vue.use(Icon);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Avatar);
