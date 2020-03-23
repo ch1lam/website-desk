bc12bc9b590542235ec89a06b6ae7babac43d77d
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-03-24 00:28:44 +0800

    配置布局

---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nvabar">
+    <span>首页</span>
+    <div class="right-menu">
+      <!-- <el-link type="primary">登陆</el-link>
+      <el-link>注册</el-link>-->
+      <el-dropdown>
+        <div class="avatar-wrapper">
+          <el-avatar>user</el-avatar>
+          <i class="el-icon-caret-bottom" />
+        </div>
+
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>切换账户</el-dropdown-item>
+          <el-dropdown-item>注销</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nvabar {
+  text-align: justify;
+}
+.right-menu {
+  float: right;
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box">
+    <el-container>
+      <el-header>
+        <navbar></navbar>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import navbar from "./components/Navbar";
+
+export default {
+  components: {
+    navbar
+  }
+};
+</script>
+
+<style scoped>
+.box,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+}
+.el-main {
+  background-color: #e9eef3;
+  text-align: center;
+  padding: 0px;
+  margin: 0px;
+}
+</style>
