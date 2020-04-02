4b6e70df2717324af1c8e8e2b502c436006bf791
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-04-02 23:25:08 +0800

    update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="box">
-    <span>这是白版</span>
+    <video-player :options="videoOptions" />
   </div>
 </template>
 
 <script>
-export default {};
-</script>
+import VideoPlayer from "@/components/VideoPlayer.vue";
 
+export default {
+  name: "VideoExample",
+  components: {
+    VideoPlayer
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            // src: "https://v-cdn.zjol.com.cn/280443.mp4",
+            src: "../../videos/280443.mp4",
+            type: "video/mp4"
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
 <style scoped>
-.box{
+.box {
   height: 100%;
 }
 </style>
