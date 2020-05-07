87723d562f7ccfe9b6502f8f239f8d7165fa7f58
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-05-08 00:17:27 +0800

    配置vuex.store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,53 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const state = {
+  username: window.sessionStorage.getItem("username"),
+  token: window.sessionStorage.getItem("token"),
+};
+
+// 实时监听 state 值的变化(最新状态)
+const getters = {
+  getUsername(state) {
+    return state.username;
   },
-  mutations: {
+  getToken() {
+    return state.token;
   },
-  actions: {
+};
+
+// 改变state值
+// 同步
+const mutations = {
+  setToken(state, newtoken) {
+    //自定义改变 state 初始值的方法，这里面的参数除了 state 之外还可以再传额外的参数(变量或对象);
+    state.token = newtoken;
+    window.sessionStorage.setItem("token", newtoken);
   },
-  modules: {
-  }
-})
+  setUsername(state, newUsername) {
+    state.username = newUsername;
+    window.sessionStorage.setItem("username", newUsername);
+  },
+};
+
+// 自定义触发 mutations 里函数的方法， context 与 store 实例具有相同方法和属性
+// 异步
+const actions = {
+  setToken(context, token) {
+    context.commit("setToken", token);
+  },
+  setUsername(context, username) {
+    context.commit("setUsername", username);
+  },
+};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
+
+export default store;
