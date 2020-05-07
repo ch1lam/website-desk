28c57634e154bf15bbb3df5747acd2eb516127b1
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-05-08 00:15:47 +0800

    加入背景

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,6 @@ body,
   margin: 0px;
   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  background: linear-gradient(to bottom right, #C6FFDD, #FBD786,#f7797d);
 }
 </style>
