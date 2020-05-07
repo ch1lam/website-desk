861e3e25598de91abde5b19cf4cc1319b0409e0b
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-05-08 00:17:13 +0800

    调整布局

--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="box">
-    <el-card shadow="hover">
-      <div class="img-container">
-        <el-image style="width: 100px; height: 100px" :fit="fill" :src="url" />
-      </div>
-
-      <div style="padding:14px">
-        <span>《马克思主义原理》</span>
-        <div class="bottom">
-          <span>主讲人:xxxx</span>
+  <el-row class="box">
+    <el-col :span="4"></el-col>
+    <el-col :span="16" class="board">
+      <el-card shadow="hover">
+        <div class="img-container">
+          <el-image style="width: 100px; height: 100px" :src="url" fit="cover" />
         </div>
-      </div>
-    </el-card>
-    <el-card shadow="hover">
-      <img src="@/assets/mao.jpg" :fit="contain" class="image" />
-      <span>《毛泽东理论概论》</span>
-    </el-card>
-    <el-card shadow="hover">
-      <span>《近代史纲要》</span>
-    </el-card>
-  </div>
+
+        <div style="padding:14px">
+          <span>《马克思主义原理》</span>
+          <div class="bottom">
+            <span>主讲人:xxxx</span>
+          </div>
+        </div>
+      </el-card>
+      <el-card shadow="hover">
+        <div>
+          <img src="@/assets/mao.jpg" class="image" style="width:100%" />
+        </div>
+
+        <span>《毛泽东理论概论》</span>
+      </el-card>
+      <el-card shadow="hover">
+        <span>《近代史纲要》</span>
+      </el-card>
+    </el-col>
+    <el-col :span="4"></el-col>
+  </el-row>
 </template>
 
 <script>
@@ -38,14 +45,19 @@ export default {
   display: flex;
   justify-content: center;
 }
+.board {
+  display: flex;
+  justify-content: center;
+  background: rgba(255, 255, 255, 0.75);
+}
 .el-card {
   margin: 20px;
   height: 240px;
   width: 240px;
 }
-.image {
-  object-fit: cover;
-}
+/* .image {
+  object-fit: none;
+} */
 .el-card >>> .el-card__body {
   padding: 0%;
 }
