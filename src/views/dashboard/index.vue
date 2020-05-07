77b4b80ae29f92aee633fbc40b22be26482d2959
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-05-08 00:17:04 +0800

    Update index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="box">
-      <el-col :span="20">
-        <video-player :options="videoOptions" />
-      </el-col>
-      <el-col :span="4">
-        <video-list />
-      </el-col>
+    <el-col :span="20">
+      <video-player :options="videoOptions" />
+    </el-col>
+    <el-col :span="4">
+      <video-list />
+    </el-col>
   </div>
 </template>
 
@@ -24,10 +24,11 @@ export default {
       videoOptions: {
         autoplay: true,
         controls: true,
+        loop: true,
         sources: [
           {
             // src: "https://v-cdn.zjol.com.cn/280443.mp4",
-            // src: "http://127.0.0.1:9999/guest/video",
+            src: "http://127.0.0.1:9999/guest/video",
             type: "video/mp4"
           }
         ]
