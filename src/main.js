076c6c9d592c94119cde8ee983029b55aef603f8
chilam <39334548+ch1lamLee@users.noreply.github.com>
2020-05-14 23:48:23 +0800

    更新elementui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./plugins/element.js";
+import 'element-ui/lib/theme-chalk/index.css'
+
 import router from "./router";
 import store from "./store";
 import "./plugins/vchart.js"
--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -17,7 +17,7 @@
         <el-button @click="download">下载</el-button>
       </div>
     </el-upload>
-    <el-table :data="tableData" stripe="style=width:100%">
+    <el-table :data="tableData" stripe style="width: 100%">
       <el-table-column prop="date" label="日期" width="180"></el-table-column>
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
