f0a06e434b36b5ba0b36f24351d903913e036604
chilam <39334548+ch1lam@users.noreply.github.com>
2022-05-20 20:11:15 +0800

    :wrench: chore: 配置element Plus按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Description  : vite config
+ * @Author       : ch1lam
+ * @Date         : 2022-05-20 15:28:59
+ * @LastEditTime : 2022-05-20 17:32:56
+ * @LastEditors  : chilam
+ * @FilePath     : \website-desk\vite.config.ts
+ */
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src/",
+    },
+  },
+});
