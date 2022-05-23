94a7561238c320868d51043492291d0239efc64b
chilam <chilam.Lee@outlook.com>
2022-05-23 19:24:56 +0800

    :construction: chore: 配置store

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-05-20 15:28:59
- * @LastEditTime : 2022-05-20 20:02:27
+ * @LastEditTime : 2022-05-23 18:57:57
  * @LastEditors  : chilam
  * @FilePath     : \website-desk\src\main.ts
  */
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
