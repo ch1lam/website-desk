c232e2f52c21ef68d70b387844268488e7ef47a3
chilam <chilam.Lee@outlook.com>
2022-05-24 19:05:51 +0800

    :construction: feat: 添加基本layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,30 +1,43 @@
 <!--
- * @Description  :
+ * @Description  : layout
  * @Author       : ch1lam
  * @Date         : 2022-05-20 17:22:10
- * @LastEditTime : 2022-05-20 18:01:38
+ * @LastEditTime : 2022-05-23 19:21:39
  * @LastEditors  : chilam
  * @FilePath     : \website-desk\src\layout\index.vue
 -->
 <template>
-  <div>
-    HelloWorlds
+  <div class="box">
+    <el-container>
+      <el-header height="65px">
+        <Navbar></Navbar>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
-
-
-    return {
-
-    }
-  }
-})
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <style scoped>
+.box,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
+
+.el-header {
+  padding: 0%;
+}
+
+.el-main {
+  text-align: center;
+  padding: 0px;
+  margin: 0px;
+}
 </style>
