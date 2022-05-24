b2e482033e380e776a416967c86f123515509c62
chilam <chilam.Lee@outlook.com>
2022-05-24 19:19:03 +0800

    :lipstick: feat: 更改全局样式

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-05-20 15:28:49
- * @LastEditTime : 2022-05-20 20:11:45
+ * @LastEditTime : 2022-05-24 15:37:49
  * @LastEditors  : chilam
  * @FilePath     : \website-desk\src\App.vue
 -->
@@ -18,11 +18,11 @@
 
 <style>
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
