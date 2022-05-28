80c5e4362cc80aab0d901d2a99bbcf036cec327d
chilam <chilam.Lee@outlook.com>
2022-05-29 01:15:37 +0800

    :lipstick: chore: 统一字体

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-05-20 15:28:49
- * @LastEditTime : 2022-05-27 23:14:48
+ * @LastEditTime : 2022-05-29 01:11:00
  * @LastEditors  : chilam
  * @FilePath     : \website-desk\src\App.vue
 -->
@@ -18,7 +18,8 @@
 #app {
   width: 100%;
   height: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
