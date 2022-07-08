db0da7d7c3a9243d423d7208f4b3e4ec983826f4
chilam <chilam.Lee@outlook.com>
2022-07-08 19:15:57 +0800

    :sparkles: feat: 封装axios

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,20 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axiosRetry from "axios-retry";
 
-const request = () => {};
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8080/api",
+  timeout: 5000,
+});
+axiosRetry(instance, { retries: 3 });
+
+const request = async (
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse> => {
+  const response = await instance.request({ url, ...config });
+  const result = response.data;
+
+  return result;
+};
 
 export { request };
