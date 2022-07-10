3956901a2e0c026f83097fd41debed1e574d3b8f
chilam <chilam.Lee@outlook.com>
2022-07-10 22:56:45 +0800

    :sparkles: feat: dashboard页

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDivider: typeof import('element-plus/es')['ElDivider']
@@ -23,6 +24,7 @@ declare module '@vue/runtime-core' {
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElTree: typeof import('element-plus/es')['ElTree']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,97 @@
 <template>
-  <div>
-    dashboard
+  <div class="box">
+    <el-col :span="18" class="videoBox">
+      <video id="myVideo" class="video-js vjs-big-play-centered" ref="video" width="800" height="600" preload="auto"
+        autoplay controls></video>
+    </el-col>
+    <el-col :span="6" class="tree_wrapper">
+      <el-tree :data="chapters" :props="defaultProps" :highlight-current="true" :default-expand-all="true"
+        :expand-on-click-node="false" @node-click="handleNodeClick" ref="tree"></el-tree>
+      <!--
+      <video-list :chapters="chapters" />-->
+    </el-col>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
+<script setup lang="ts">
+import { id } from "element-plus/es/locale";
+import { ref, reactive, onMounted } from "vue";
 
 
-    return {}
+const videoOptions = reactive({
+  autoplay: false,
+  height: 600,
+  width: 800,
+  controls: true,
+  preload: "auto",
+  loop: true,
+  sources: [
+    {
+      src: "",
+      type: "video/mp4"
+    }
+  ]
+})
+
+const defaultProps = reactive({
+  id: "id",
+  sequence: "sequence",
+  children: "children",
+  videoId: "videoId",
+  label: (data: any) => {
+    return data.sequence + ". " + data.label;
   }
 })
+const currentChapterId = ref("");
+const chapters = ref([]);
+
+onMounted(() => { getCurrentChapterId() })
+
+const getCurrentChapterId = () => {
+  // TODO
+}
+
+const getAllChapter = (id:number) => {
+  // TODO request
+}
+
+const handleNodeClick = () => {
+  // TODO change video src
+}
+
 </script>
 
 <style scoped>
+.box {
+  height: 100%;
+}
+.el-col {
+  height: 100%;
+}
+.el-tree-node > .el-tree-node__children {
+  overflow: visible;
+}
+.el-tree {
+  min-width: 100%;
+  display: inline-block !important;
+}
+.videoBox {
+  display: flex;
+  justify-content: center;
+}
+.tree_wrapper {
+  overflow-y: auto;
+  overflow-x: scroll;
+  height: 100%;
+  background-color: #ffffff;
+  background-color: white;
+  padding: 30px;
+  border-right-style: solid;
+  border-width: 1px;
+  border-color: gray;
+}
+.video-js {
+  width: 800px;
+  height: 600px;
+}
 </style>
