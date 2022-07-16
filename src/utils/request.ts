b2d0f312012dbb4c4fa5267d036b12a4f1d71286
chilam <chilam.Lee@outlook.com>
2022-07-16 23:06:20 +0800

    :goal_net: feat: request错误捕捉

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,21 +1,22 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import axiosRetry from "axios-retry";
+import { getToken } from "./auth";
 
 const instance = axios.create({
   baseURL: "/api",
   timeout: 5000,
+  headers: { Authorization: getToken() ?? "" },
 });
 
 axiosRetry(instance, { retries: 3 });
 
-const request = async (
-  url: string,
-  config?: AxiosRequestConfig
-): Promise<AxiosResponse> => {
-  const response = await instance.request({ url, ...config });
-  const result = response.data;
+const request = async (url: string, config?: AxiosRequestConfig) => {
+  const response = await instance
+    .request({ url, ...config })
+    .then((res) => [null, res.data])
+    .catch((err) => [err, null]);
 
-  return result;
+  return response;
 };
 
 export { request };
