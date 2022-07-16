a1f6d1bd4d3131329d3385fd0272938b2ef532ef
chilam <chilam.Lee@outlook.com>
2022-07-16 23:07:05 +0800

    :goal_net: feat: 登录逻辑异常处理

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,15 +1,28 @@
+import { ElMessage } from "element-plus";
+import { setToken } from "../utils/auth";
 import { request } from "../utils/request";
 
-const loginReq = (username: string, password: string) => {
-  return request("/login", {
+const loginReq = async (username: string, password: string) => {
+  const [err, data] = await request("/login", {
     method: "post",
     data: {
       username: username,
       password: password,
     },
-  })
-    .then((data) => [null, data])
-    .catch((err) => [err, null]);
+  });
+
+  if (err) {
+    ElMessage({
+      showClose: true,
+      type: "error",
+      message: err.response.data.message,
+      center: true,
+    });
+  } else {
+    setToken(data.data.token);
+  }
+
+  return data;
 };
 
 export { loginReq };
